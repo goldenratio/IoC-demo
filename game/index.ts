208983da0115d526edfc63ad7478ffd771f466b9
karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>
2018-10-10 10:41:16 +0300

    removed game source

--- game/index.ts
+++
@@ -1,4 +0,0 @@
-import { bootstrapModule } from '../src/bootstrap/bootstrap-module';
-import { GameModuleConfig } from './GameModuleConfig';
-
-bootstrapModule(GameModuleConfig);
--- game/types/index.ts
+++
@@ -1,3 +0,0 @@
-export const GAME_TYPES = {
-  GameModel: Symbol.for('GameModel')
-};
